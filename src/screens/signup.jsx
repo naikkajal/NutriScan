3026864e2a531f7d66373b5ce362581b98d5b83e
Kajal Naik <naikkajal0603@gmail.com>
2024-07-24 22:50:00 +0530

    img

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,60 +1,73 @@
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Entypo from '@expo/vector-icons/Entypo';
+import { Fontisto } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
-import { auth } from '../../firebase';  
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const navigation = useNavigation();
-
   const handleRegister = () => {
     navigation.navigate("Login");
-  };
-
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(userCredentials => {
-        const user = userCredentials.user;
-        console.log('Registered with:', user.email);
-      })
-      .catch(error => alert(error.message));
-  };
+  }
 
   return (
-    <View style={styles.createcontainer}>
-      <Text style={styles.createtext}>Create Account</Text>
-      <Text style={styles.startedtext}>Just a few quick things to get started</Text>
-      <View style={styles.usercontainer}>
+    <View style={styles.content}>
+      <Image source={require("../src/images/topimg.png")} style={styles.image} />
+      <Text style={styles.signup}>Create Account</Text>
+      <Text style={styles.getstarted}>Just a few quick things to get started</Text>
+      <View style={styles.usernamecontainer}>
+        <Entypo name="user" size={20} color="black" style={styles.usericon} />
         <TextInput
-          style={styles.usertext}
+          style={styles.usernametext}
+          placeholder='Username'
+          placeholderTextColor={"#888"}
+        />
+      </View>
+      <View style={styles.emailcontainer}>
+        <Fontisto name="email" size={24} color="black" style={styles.emailicon} />
+        <TextInput
+          style={styles.emailtext}
           placeholder='Email'
-          placeholderTextColor={"black"}
-          onChangeText={text => setEmail(text)}
-          value={email}
+          placeholderTextColor={"#888"}
         />
       </View>
-      <View style={styles.passcontainer}>
+      <View style={styles.mobilecontainer}>
+        <Entypo name="mobile" size={24} color="black" style={styles.usericon} />
         <TextInput
-          style={styles.usertext}
-          secureTextEntry
-          placeholder='Password'
-          placeholderTextColor={"black"}
-          onChangeText={text => setPassword(text)}
-          value={password}
+          style={styles.mobiletext}
+          placeholder='Mobile No.'
+          placeholderTextColor={"#888"}
         />
       </View>
-      <Text style={styles.forgottext}>Forgot Password?</Text>
-      <View style={styles.signinbutton}>
-        <TouchableOpacity onPress={handleSignUp}>
-          <Text style={styles.signintext}>Sign Up</Text>
-        </TouchableOpacity>
+      <View style={styles.passwordcontainer}>
+        <Entypo name="lock" size={20} color="black" style={styles.usericon} />
+        <TextInput
+          style={styles.passwordtext}
+          placeholder='Password'
+          placeholderTextColor={"#888"}
+        />
       </View>
       <View>
+        <Text style={styles.forgotcontainer}>Forgot your password?</Text>
+      </View>
+      <View style={styles.createcontainer}>
+        <TouchableOpacity onPress={handleRegister} style={styles.signinButton}>
+          <LinearGradient
+            colors={['#8A2BE2', '#FF1493']}
+            style={styles.gradientButton}
+            start={[0, 0]}
+            end={[1, 1]}
+          >
+            <Text style={styles.signinText}>Sign In</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.alhaveacccountainer}>
         <TouchableOpacity onPress={handleRegister}>
-          <Text style={styles.alreadytext}>
-            Already have an Account?<Text style={{ textDecorationLine: "underline" }}> Sign In</Text>
+          <Text style={styles.alhaveacctext}>
+            Already have an account? <Text style={{ textDecorationLine: "underline", color: "darkblue", fontWeight: "bold" }}>Sign In</Text>
           </Text>
         </TouchableOpacity>
       </View>
@@ -62,64 +75,123 @@ const Signup = () => {
   );
 }
 
-export default Signup;
+export default Signup
 
 const styles = StyleSheet.create({
-  createcontainer: { alignContent: "center" },
-  createtext: {
-    textAlign: "center",
-    marginTop: 160,
-    fontSize: 35,
-    fontWeight: "bold"
-  },
-  startedtext: {
-    textAlign: "center",
-    marginTop: 10,
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  usercontainer: {
-    backgroundColor: "lightgrey",
-    flexDirection: "row",
-    marginTop: 70,
-    height: 60,
-    marginHorizontal: 50,
-    borderRadius: 50
-  },
-  usertext: {
-    marginLeft: 30,
+  content: {
+    backgroundColor: "white",
     flex: 1
   },
-  passcontainer: {
-    backgroundColor: "lightgrey",
-    flexDirection: "row",
-    marginTop: 40,
-    height: 60,
-    marginHorizontal: 50,
-    borderRadius: 50
+  image: {
+    width: "120%",
+    height: "20%"
   },
-  forgottext: {
-    textAlign: "right",
-    marginRight: 60,
+  signup: {
+    fontSize: 31,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: 40
+  },
+  getstarted: {
+    textAlign: "center",
     marginTop: 15,
-    color: "darkblue"
+    fontWeight: "500",
+    fontSize: 17
   },
-  signinbutton: {
-    backgroundColor: "firebrick",
-    height: 50,
-    marginTop: 30,
-    marginHorizontal: 50,
+  usernamecontainer: {
+    backgroundColor: "#f1f1f1",
+    flexDirection: "row",
     borderRadius: 50,
+    marginHorizontal: 50,
+    marginTop: 30,
+    elevation: 10,
+    height: 50,
     alignItems: "center",
   },
-  signintext: {
-    textAlign: "center",
-    color: "white",
-    fontSize: 20,
-    marginVertical: 10
+  usernametext: { textAlign: "center" },
+  emailcontainer: {
+    backgroundColor: "#f1f1f1",
+    flexDirection: "row",
+    borderRadius: 50,
+    marginHorizontal: 50,
+    marginTop: 30,
+    elevation: 10,
+    height: 51,
+    alignItems: "center",
   },
-  alreadytext: {
+  usericon: {
+    marginHorizontal: 20
+  },
+  emailicon: { marginHorizontal: 20 },
+  mobileicon: {},
+  emailtext: {
+    textAlign: "center"
+  },
+  mobilecontainer: {
+    backgroundColor: "#f1f1f1",
+    flexDirection: "row",
+    borderRadius: 50,
+    marginHorizontal: 50,
+    marginTop: 30,
+    elevation: 10,
+    height: 50,
+    alignItems: "center",
+  },
+  mobiletext: { textAlign: "center" },
+  passwordcontainer: {
+    backgroundColor: "#f1f1f1",
+    flexDirection: "row",
+    borderRadius: 50,
+    marginHorizontal: 50,
+    marginTop: 32,
+    elevation: 10,
+    height: 50,
+    alignItems: "center",
+  },
+  passwordtext: { textAlign: "center" },
+  forgotcontainer: {
+    textAlign: "right",
+    marginRight: 55,
+    marginVertical: 5,
+    color: "mediumblue",
+    fontWeight: "400",
+    fontSize: 15,
+    marginTop: 15
+  },
+  createcontainer: {
+    backgroundColor: "#f1f1f1",
+    flexDirection: "row",
+    borderRadius: 50,
+    marginHorizontal: 50,
+    marginTop: 30,
+    elevation: 10,
+    height: 50,
+    alignItems: "center",
+  },
+  signinButton: {
+    flex: 1,
+    borderRadius: 50,
+  },
+  gradientButton: {
+    flex: 1,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signinText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  alhaveacccountainer: {
+    marginTop: 75,
+    alignSelf: 'center',
+  },
+  alhaveacctext: {
     textAlign: "center",
-    marginTop: 200
-  }
+    fontSize: 17,
+    fontWeight: "300",
+    alignSelf: 'center',
+  },
 });
+
