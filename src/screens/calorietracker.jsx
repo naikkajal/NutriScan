58f8e894abcbc90aee810d3fc51e8b536ba018f9
Kajal Naik <naikkajal0603@gmail.com>
2024-08-05 21:52:27 +0530

    final calculations

--- src/screens/calorietracker.jsx
+++ src/screens/calorietracker.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import axios from 'axios';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
 
 const TrackerScreen = () => {
   const [height, setHeight] = useState('');
@@ -23,110 +24,129 @@ const TrackerScreen = () => {
         age: Number(age),
         gender,
         activityLevel,
-        email, // Add email to the request payload
+        email,
       });
       setDailyCalorieIntake(response.data.dailyCalorieIntake);
       navigation.navigate('CalorieResult', { dailyCalorieIntake: response.data.dailyCalorieIntake });
     } catch (error) {
       console.error(error);
+      Alert.alert('Error', 'An error occurred while calculating calorie intake.');
     }
   };
 
-  const handleLogin = () => {
-    if (!email || !password) {
-      Alert.alert('Error', 'Please fill in both fields.');
-      return;
-    }
-    const loginData = { email, password };
-  
-    axios.post('http://192.168.1.104:5011/login', loginData)
-      .then(res => {
-        if (res.data.status === 'ok') {
-          if (res.data.profileCompleted) {
-            navigation.navigate('Home');
-          } else {
-            navigation.navigate('Main', { screen: 'Capture' });
-          }
-        } else {
-          Alert.alert('Error', JSON.stringify(res.data));
-        }
-      })
-      .catch(error => {
-        Alert.alert('Error', error.message);
-      });
-  };
-  
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false} keyboardShouldPersistTaps={'always'} style={{ backgroundColor: 'white' }}>
-      <View style={styles.content}>
-        <Image source={require("../images/topimg.png")} style={styles.image} />
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false} keyboardShouldPersistTaps='always' style={{ backgroundColor: 'white' }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Track Your Daily Calorie Intake</Text>
         
-        <View style={styles.formContainer}>
-          <Text style={styles.signup}>Welcome Back</Text>
-          <Text style={styles.getstarted}>Log in to your account</Text>
-          
-          <View style={styles.inputContainer}>
-            <Fontisto name="email" size={20} color="black" style={styles.icon} />
-            <TextInput
-              accessibilityLabel="Email Input"
-              style={styles.inputText}
-              placeholder='Email'
-              placeholderTextColor={"#888"}
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-            />
-          </View>
-          
-          <View style={styles.inputContainer}>
-            <Entypo name="lock" size={20} color="black" style={styles.icon} />
-            <TextInput
-              accessibilityLabel="Password Input"
-              style={styles.inputText}
-              placeholder='Password'
-              placeholderTextColor={"#888"}
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-            />
-          </View>
-          
-          <View style={styles.forgotPasswordContainer}>
-            <Text style={styles.forgotPassword}>Forgot your password?</Text>
-          </View>
-          
-          <TouchableOpacity onPress={handleLogin} style={styles.buttonContainer}>
-            <LinearGradient
-              colors={['#8A2BE2', '#FF1493']}
-              style={styles.gradientButton}
-              start={[0, 0]}
-              end={[1, 1]}
-            >
-              <Text style={styles.buttonText}>Login</Text>
-            </LinearGradient>
-          </TouchableOpacity>
-          
-          <TouchableOpacity onPress={() => navigation.navigate('Signup')} style={styles.signupRedirect}>
-            <Text style={styles.signupRedirectText}>
-              Don't have an account? <Text style={{textDecorationLine: "underline", color: "darkblue", fontWeight: "bold" }}>Sign Up</Text>
-            </Text>
-          </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor="#888"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
         </View>
+        
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Height (cm)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Height'
+            placeholderTextColor="#888"
+            value={height}
+            onChangeText={setHeight}
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Weight (kg)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Weight'
+            placeholderTextColor="#888"
+            value={weight}
+            onChangeText={setWeight}
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Age</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Age'
+            placeholderTextColor="#888"
+            value={age}
+            onChangeText={setAge}
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Gender</Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={gender}
+              onValueChange={(itemValue) => setGender(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Select Gender" value="" />
+              <Picker.Item label="Male" value="male" />
+              <Picker.Item label="Female" value="female" />
+            </Picker>
+          </View>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Activity Level</Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={activityLevel}
+              onValueChange={(itemValue) => setActivityLevel(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Select Activity Level" value="" />
+              <Picker.Item label="Sedentary" value="sedentary" />
+              <Picker.Item label="Lightly Active" value="lightly_active" />
+              <Picker.Item label="Moderately Active" value="moderately_active" />
+              <Picker.Item label="Very Active" value="very_active" />
+              <Picker.Item label="Extra Active" value="extra_active" />
+            </Picker>
+          </View>
+        </View>
+
+        <TouchableOpacity onPress={calculateCalorieIntake} style={styles.buttonContainer}>
+          <LinearGradient
+            colors={['#8A2BE2', '#FF1493']}
+            style={styles.gradientButton}
+            start={[0, 0]}
+            end={[1, 1]}
+          >
+            <Text style={styles.buttonText}>Calculate</Text>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
-};  
+};
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
-    marginTop:40
+    textAlign: 'center',
+    marginTop:50
   },
   inputContainer: {
     marginBottom: 15,
@@ -134,42 +154,31 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 16,
     fontWeight: '600',
+    marginBottom: 5,
   },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
     padding: 10,
     borderRadius: 5,
+    fontSize: 16,
   },
-  radioGroup: {
-    flexDirection: 'row',
-    marginVertical: 10,
-  },
-  radioButton: {
+  pickerContainer: {
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    overflow: 'hidden',
   },
-  radioButtonSelected: {
-    backgroundColor: '#4c669f',
-    borderColor: '#4c669f',
-  },
-  radioText: {
-    color: '#333',
-  },
-  radioTextSelected: {
-    color: '#fff',
+  picker: {
+    height: 50,
+    width: '100%',
   },
   buttonContainer: {
     borderRadius: 5,
     overflow: 'hidden',
     marginVertical: 20,
   },
-  button: {
+  gradientButton: {
     paddingVertical: 15,
     justifyContent: 'center',
     alignItems: 'center',
@@ -183,6 +192,8 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
     color: '#333',
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
 
